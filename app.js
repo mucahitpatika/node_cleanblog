9c47f0741428926fff9a299e10f6be90e7161d8a
Mücahit Tiryaki <mucahittiryaki11@outlook.com>
2023-06-12 18:15:57 +0300

    static files are added

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const app = express();
+const ejs = require('ejs');
 
 const port = 3000;
-app.get('/', (req, res) => {
-	const blog = {
-		id: 1,
-		title: 'blog title',
-		description: 'Blog description',
-	};
 
-	res.send(blog);
+//Template Engine
+app.set('view engine', 'ejs');
+
+//Middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Routes
+app.get('/', (req, res) => {
+	res.render('index');
+});
+app.get('/about', (req, res) => {
+	res.render('about');
+});
+app.get('/add_post', (req, res) => {
+	res.render('add_post');
+});
+app.get('/post', (req, res) => {
+	res.render('post');
 });
 
 app.listen(port, () => {
